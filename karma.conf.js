d77175e7aecf224ea25af6f5d53dda213c7149ba
rpereda <raul.pereda@beauthentic.digital>
2017-06-06 15:42:27 -0500

    Added support for jenkins

--- karma.conf.js
+++ karma.conf.js
@@ -38,8 +38,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+    reporters: ['dots', 'junit'],
 
+    junitReporter: {
+      outputFile: 'test-results.xml'
+    },
 
     // web server port
     port: 9876,
@@ -60,12 +63,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['slimerjs'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
